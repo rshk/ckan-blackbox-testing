4ab8ca13770e47755278324038849bbe61c0f5c6
Samuele Santi <redshadow@hackzine.org>
2013-12-06 13:29:23 +0100

    More work on fixtures

--- conftest.py
+++ conftest.py
@@ -1,1 +1,6 @@
-from fixtures.ckan_env import *
+import os
+
+if os.environ.get('TRAVIS'):
+    from fixtures.ckan_env import *  # noqa
+else:
+    raise RuntimeError("Unable to figure out which fixtures to load.")

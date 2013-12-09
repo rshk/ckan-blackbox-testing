10a66101eec03d3a8b22f52cbc292af5df2f432d
Samuele Santi <redshadow@hackzine.org>
2013-12-09 15:07:28 +0100

    Added user creation via paster command

--- api_testing/test_api_simple.py
+++ api_testing/test_api_simple.py
@@ -11,3 +11,18 @@ def test_api_simple(running_ckan):
     assert response.ok
     assert response.status_code == 200
     assert response.json()['result'] == []
+
+    ## Create User (we will need this later..)
+    running_ckan.paster_with_conf(
+        'user', 'add', 'admin', 'password=admin', 'email=admin@example.com',
+        'api-key=my-api-key')
+
+    # todo: create a dataset
+    # todo: retrieve it and check that it matches
+    # todo: make sure it is listed correctly
+    # todo: update the dataset
+    # todo: make sure the updates had effect
+    # todo: remove the dataset
+    # todo: make sure the dataset was deleted correctly
+    # todo: get must return 404
+    # todo: list must be empty
---
+++ fixtures/ckan_env_local.py
@@ -0,0 +1,12 @@
+"""
+Fixtures for running tests locally.
+
+This allows running tests in a ready environment, thus saving
+up some time (no virtualenv download + installation time).
+
+For this we need:
+
+- a virtualenv containing an installation of Ckan
+- an administrative access to a postgresql database
+- some way to clear indices on a Solr installation!!
+"""

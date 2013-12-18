31f99a7c50e8efe9790d25032d36201bc4728eb7
Samuele Santi <redshadow@hackzine.org>
2013-12-18 17:07:06 +0100

    f** this

--- simple_api_testing/travis-run.sh
+++ simple_api_testing/travis-run.sh
@@ -6,5 +6,5 @@ export CKAN_VIRTUALENV=$VIRTUAL_ENV
 export CKAN_POSTGRES_ADMIN=postgresql://postgres:pass@localhost/postgres
 export CKAN_SOLR=http://localhost:8983/solr/ckan-2.0
 
-#exec python ./run_tests.py
-exec pytest --confcutdir="$PWD" --verbose -rsxX ./tests/
+exec python ./run_tests.py
+#exec py.test --confcutdir="$PWD" --verbose -rsxX ./tests/
